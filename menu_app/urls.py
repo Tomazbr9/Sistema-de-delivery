b20c4cbe03c82d3f041a2d1f4a1ddfb5f2fb4d15
Bruno Tomaz <tomazcraft@gmail.com>
2024-11-22 11:34:24 -0300

    Criei uma view para remover os items do carrinho

--- menu_app/urls.py
+++ menu_app/urls.py
@@ -6,5 +6,9 @@ app_name = 'menu_app'
 urlpatterns = [
     path('menu/', views.index, name='menu'),
     path('category/<int:category_id>/', views.category_by_products_view, name='category'),
-    path('product/<int:product_id>/', views.product_detail, name='product')
+    path('product/<int:product_id>/', views.product_detail, name='product'),
+
+    path('cart/', views.view_cart, name='cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add'),
+    path('delete/<int:product_id>/', views.delete_item, name='delete')
 ]
--- menu_app/views/menu_view.py
+++ menu_app/views/menu_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from menu_app.models import Category, Product
 
 
@@ -26,3 +26,52 @@ def product_detail(request, product_id):
             'product': product
     }
     return render(request, 'product.html', context)
+
+def add_to_cart(request, product_id):
+    
+    product = Product.objects.get(pk=product_id)
+
+    quantify = int(request.POST.get('quantify', 1))
+
+    cart = request.session.get('cart', {})
+
+    if str(product.pk) in cart:
+        cart[str(product.pk)]['quantify'] += quantify
+    else:
+        cart[str(product.pk)] = {
+            'name': product.name,
+            'price': str(product.price),
+            'quantify': quantify,
+            'image': product.image.url if product.image else '',
+        }
+    
+    request.session['cart'] = cart
+
+    return redirect('menu_app:cart')
+
+def view_cart(request):
+
+    cart = request.session.get('cart', {})
+    total = 0
+    for item in cart.values():
+        total += float(item['price']) * item['quantify']
+    
+    context = {
+        'cart': cart,
+        'total': total
+    }
+
+    return render(request, 'cart.html', context)
+
+def delete_item(request, product_id):
+    cart = request.session.get('cart', {})
+    
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+
+    request.session['cart'] = cart
+   
+    return redirect('menu_app:cart')
+
+def check(request):
+    ...
